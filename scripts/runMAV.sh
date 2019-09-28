30a012f669dcbbc14d90b6522f9e5c5310c54057
Eduardo Marques <edrdo@dcc.fc.up.pt>
2019-09-28 23:30:51 +0100

    Some indentation

--- scripts/runMAV.sh
+++ scripts/runMAV.sh
@@ -14,5 +14,9 @@ SCRIPTS_DIR=$(cd $(dirname $(which $0)); pwd)
 echo "Running SITL simulation ..."
 echo SYS_ID=$SYS_ID GCS_HOST=$2 GCS_PORT=$3
 echo SIM_OPTIONS=$SIM_OPTIONS
-docker run --rm -it -v $SCRIPTS_DIR:/external -e "SIM_OPTIONS=--out=udpout:$GCS_HOST:$GCS_PORT --out=udpout:$GCS_HOST:14550 -m --target-system=$SYS_ID $SIM_OPTIONS" --entrypoint "/external/entryPoint.sh" gmyoungbloodparc/ardupilot-sitl $SYS_ID
+docker run --rm -it \
+  -v $SCRIPTS_DIR:/external \
+  -e "SIM_OPTIONS=--out=udpout:$GCS_HOST:$GCS_PORT --out=udpout:$GCS_HOST:14550 -m --target-system=$SYS_ID $SIM_OPTIONS" \
+  --entrypoint "/external/entryPoint.sh" \
+  gmyoungbloodparc/ardupilot-sitl $SYS_ID
 exit $?
